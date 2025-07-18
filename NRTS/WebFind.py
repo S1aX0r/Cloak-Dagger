c61fb72b2b2215d4998e7ef92b7b560c48e7f470
S1aX0r <s1ax0r@tutamail.com>
2025-07-18 00:12:54 -0400

    Add files via upload
    WebFind directory brute-forcer added to Network Recon Tools & Scripts

---
+++ NRTS/WebFind.py
@@ -0,0 +1,41 @@
+import requests
+
+def http_content_scanner(url, wordlist):
+    found_paths = []
+
+    for path in wordlist:
+        url = f"{url}/{path}"
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"[*] Found: {url}")
+            elif response.status_code == 404:
+                print(f"[X] Not Found: {url}")
+            elif response.status_code == 403:
+                print(f"[!] Forbidden: {url}")
+            else:
+                print(f"[?] Status {response.status_code}: {url}")
+        except requests.exceptions.RequestException as e:
+                print(f"[X] Error accessing {url}: {e}")
+    
+    return found_paths
+
+if __name__ == "__main__":
+    target_url = input("Enter target URL: ").strip("/")
+         
+    wordlist = [
+    "admin", "login", "uploads", "config.php", "backup", "robots.txt",
+    "index.php", "test.php", "api", "images", "css", "js", "favicon.ico"
+    ]
+
+    print(f"Running WebFind on {target_url}")
+    found = http_content_scanner(target_url, wordlist)
+
+    if found:
+        print(f"\nFound Paths: ")
+
+        for path in found:
+            print(path)
+
+    else:
+        print(f"\nNo Paths Found for: {target_url}")
