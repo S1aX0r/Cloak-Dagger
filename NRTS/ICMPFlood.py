038df3054d989deb564661dac9e2608c64bcf334
S1aX0r <s1ax0r@tutamail.com>
2025-07-18 00:27:23 -0400

    Add files via upload
    Logo added

--- NRTS/ICMPFlood.py
+++ NRTS/ICMPFlood.py
@@ -2,6 +2,16 @@ import subprocess
 import time
 import os
 
+logo = '''
+  IIIII  CCCCC  M   M  PPPP   FFFFF  L      OOO    OOO   DDDD  
+    I    C      MM MM  P   P  F      L     O   O  O   O  D   D 
+    I    C      M M M  PPPP   FFFF   L     O   O  O   O  D   D 
+    I    C      M   M  P      F      L     O   O  O   O  D   D 
+  IIIII  CCCCC  M   M  P      F      LLLLL  OOO    OOO   DDDD  
+
+'''
+print(logo)
+
 def ping_flood(target_ip, count):
     
     print(f"Pinging {target_ip} with {count} packets...")
